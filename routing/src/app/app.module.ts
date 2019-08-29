4bfd1116ac71d2590b8248f286d024872287d5cf
tiwary23 <dtiwary12@gmail.com>
2019-08-29 17:33:10 +0530

    routing and navigation

---
+++ routing/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule, routingComponent } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    routingComponent
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
